5bc6bd3179d7d47e0441f0d6212cc945d2b72265
Marc Resnick <mpresnick@gmail.com>
2025-12-09 20:32:43 -0500

    Update get_final schedule to Tuesday at 8:40 PM

--- gamedaybot/espn/scheduler.py
+++ gamedaybot/espn/scheduler.py
@@ -39,7 +39,7 @@ def scheduler():
                   day_of_week='tue', hour=20, minute=5, start_date=ff_start_date, end_date=ff_end_date,
                   timezone=my_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_final'], id='final',
-                  day_of_week='tue', hour=9, minute=45, start_date=ff_start_date, end_date=ff_end_date,
+                  day_of_week='tue', hour=20, minute=40, start_date=ff_start_date, end_date=ff_end_date,
                   timezone=my_timezone, replace_existing=True)
     sched.add_job(espn_bot, 'cron', ['get_standings'], id='standings',
                   day_of_week='wed', hour=7, minute=30, start_date=ff_start_date, end_date=ff_end_date,
